edef2b91c9e223026ae4f6e117d94269e5474eea
Emad Salehi <emad.s1178@yahoo.com>
2018-10-25 14:35:18 +0330

    View log added.

--- src/view/View.java
+++ src/view/View.java
@@ -5,18 +5,27 @@ import java.util.Scanner;
 public class View {
     private static Scanner input = new Scanner(System.in);
 
+    public String getCommand() {
+        return input.nextLine();
+    }
+
     public void logAdd(int ID) {
+        System.out.println(ID);
     }
 
     public void logSeeScore(double score) {
+        System.out.println(score);
     }
 
     public void logSeeGills(int gills) {
+        System.out.println(gills);
     }
 
     public void logNotPossible() {
+        System.out.println("not possible");
     }
 
-    public void logYield(double Score1, double Score2) {
+    public void logYield(double score1, double score2) {
+        System.out.println(score1 + " " + score2);
     }
 }
